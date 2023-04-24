a82f43e311ee98f4a3094b4eeed508457c654c13
Satalae <harrison19988@gmail.com>
2023-04-24 14:36:30 -0700

    Models finished

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
+
+const db = sequelize
 
 const app = express();
 const PORT = process.env.PORT || 3001;
